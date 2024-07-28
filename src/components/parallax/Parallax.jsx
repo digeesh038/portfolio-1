2ab060df61c09aa738477e17ea6f06325a07d33d
digeesh038 <digeesh038@gmail.com>
2024-07-28 22:41:57 +0530

    update

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,51 +1,38 @@
 import { useRef } from "react";
 import "./parallax.scss";
-import { motion,useScroll,useTransform } from "framer-motion"
-import resume from '../../digee_resume.jpg';
-const Parallex = ({type}) => {
+import { motion, useScroll, useTransform } from "framer-motion";
+import resumeImage from '../../digee_resume.jpg';
+import resumePDF from '../../digee_resume.pdf';
+const Parallax = ({ type }) => {
+    const ref = useRef();
 
-const ref = useRef()
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start start", "end start"]
+    });
 
-    const {scrollYProgress} = useScroll({
-        target:ref,
-        offset : ["start start","end start"]
-    })
+    const yText = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+    const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
 
-
-    const yText = useTransform(scrollYProgress, [0,1] ,["0%", "100%"])
-    const yBg = useTransform(scrollYProgress, [0,1] ,["0%", "100%"])
-
-return (
-    <div 
-    className="parallax"
-    ref={ref}
-    style={{
-        background :
-        type === "services" 
-        ? "linear-gradient(180deg, #111132, #0c0c1d)"
-        : "linear-gradient(180deg, #111132, #505084)",
-    }}
-    >
-        <div class="canva-container">
-            <img className = "img" src={resume} alt="" />
-        </div>
-        <div className="a">
+    return (
+        <motion.div
+            className="parallax"
+            ref={ref}
+            style={{
+                background: type === "services"
+                    ? "linear-gradient(180deg, #111132, #0c0c1d)"
+                    : "linear-gradient(180deg, #111132, #505084)",
+            }}
+        >
+            <motion.div className="canva-container" style={{ y: yBg }}>
+            <img src={resumeImage} alt="Resume" />
+            </motion.div>
+            <motion.div className="a" style={{ y: yText }}>
             <img src="/download.png" alt="" />
-            <a href="src\digee_resume.pdf" >Download CV</a>
-        </div>       
-        {/* <motion.div className="mountains"></motion.div> */}
-        {/* <motion.div
-        className="planets" 
-        style={{
-            y:yBg,
-            backgroundImage: `url(${
-                type === "services" ? "/planets.png" : "/sun.png"
-            })`,
-        }}></motion.div> */}
-        {/* <motion.div style={{x:yBg}} className="stars"></motion.div> */}
-</div>
+            <a href={resumePDF} download>Download CV</a>
+            </motion.div>
+        </motion.div>
+    );
+};
 
-)
-}
-export default Parallex
-
+export default Parallax;
