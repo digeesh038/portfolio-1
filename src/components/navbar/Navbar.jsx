8bfad6edb05c0de4a5957ea1838c3b07a138e434
digeesh038 <digeesh038@gmail.com>
2024-07-28 23:00:32 +0530

    inital

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion";
 const Navbar = () => {
 return (
     <div className="navbar">
-        {/*Sidebar*/}
         <Sidebar />
         <div className="wrapper">
             <motion.span 
@@ -14,10 +13,10 @@ return (
             >
                 Digeesh</motion.span>
             <div className="social">
-                <a href="https://github.com/digeesh038"><img src="/github-sign.png" alt="" /></a>
-                <a href="https://www.linkedin.com/in/digeesh-s-013051272/"><img src="/linkedin.png" alt="" /></a>
-                <a href="https://www.instagram.com/digee_2683/"><img src="/instagram.png" alt="" /></a>
-                <a href="https://x.com/home"><img src="/twitter.png" alt="" /></a>
+                <a href="https://github.com/digeesh038"><img src="/github-sign.png" alt="No image" /></a>
+                <a href="https://www.linkedin.com/in/digeesh-s-013051272/"><img src="/linkedin.png" alt="No image" /></a>
+                <a href="https://www.instagram.com/digee_2683/"><img src="/instagram.png" alt="No image" /></a>
+                <a href="https://x.com/home"><img src="/twitter.png" alt="No image" /></a>
             </div>
         </div>
     </div>
