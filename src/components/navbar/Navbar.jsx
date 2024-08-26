a7aa7e5061abac3bc386ff4076199ebef7edf939
digeesh038 <digeesh038@gmail.com>
2024-08-26 20:54:10 +0530

    updaet1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,7 +16,7 @@ return (
                 <a href="https://github.com/digeesh038"><img src="/github-sign.png" alt="No image" /></a>
                 <a href="https://www.linkedin.com/in/digeesh-s-013051272/"><img src="/linkedin.png" alt="No image" /></a>
                 <a href="https://www.instagram.com/digee_2683/"><img src="/instagram.png" alt="No image" /></a>
-                {/* <a href="https://x.com/digeesh_2683"><img src="/twitter.png" alt="No image" /></a> */}
+                <a href="https://x.com/digeesh_2683"><img src="/twitter.png" alt="No image" /></a>
             </div>
         </div>
     </div>
