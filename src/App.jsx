14276c72990daa4b9c834187c4361a14eacd39b6
digeesh038 <digeesh038@gmail.com>
2025-01-09 12:03:26 +0530

    update-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import "./app.scss"
-
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
-import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 
 const App = () => {
@@ -15,8 +13,6 @@ const App = () => {
       </section>
     <section id="Resume"><Parallax type="services" /></section>
     <section id="Projects"><Services /></section>
-    {/* <section ><Parallax  type="portfolio"/></section> */}
-    {/* <Portfolio /> */}
     <section id="Contact"><Contact /></section>
     </div>;
 };
