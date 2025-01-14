9a8f0bb970175551f09b2db18e9c4ae705f4c148
digeesh038 <digeesh038@gmail.com>
2025-01-14 14:15:36 +0530

    updater

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import "./parallax.scss";
 import { motion, useScroll, useTransform } from "framer-motion";
-import resumeImage from '../../digee_resume.png';
+import resumeImage from '../../digee_resume.jpg';
 import resumePDF from '../../digee_resume.pdf';
 const Parallax = ({ type }) => {
     const ref = useRef();
