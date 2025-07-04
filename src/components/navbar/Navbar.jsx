6e281ca444ba792e23f49af36989802f1b75d954
digeesh038 <digeesh038@gmail.com>
2025-07-04 18:02:27 +0530

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,25 +2,25 @@ import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss"
 import { motion } from "framer-motion";
 const Navbar = () => {
-return (
-    <div className="navbar">
-        <Sidebar />
-        <div className="wrapper">
-            <motion.span 
-            inital ={{opacity:0,scale:0.5}} 
-            animate  ={{opacity:1,scale:1}}
-            transition={{duration:0.5}}
-            >
-                Digeesh</motion.span>
-            <div className="social">
-                <a href="https://github.com/digeesh038" target="_blank"><img src="/github-sign.png" alt="No image" /></a>
-                <a href="https://www.linkedin.com/in/digeesh-s/" target="_blank"><img src="/linkedin.png" alt="No image" /></a>
-                <a href="https://www.instagram.com/digee_2683/" target="_blank"><img src="/instagram.png" alt="No image" /></a>
-                <a href="https://x.com/digeesh_2683" target="_blank"><img src="/twitter.png" alt="No image" /></a>
+    return (
+        <div className="navbar">
+            <Sidebar />
+            <div className="wrapper">
+                <motion.span
+                    inital={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                >
+                    Digeesh</motion.span>
+                <div className="social">
+                    <a href="https://github.com/digeesh038" target="_blank"><img src="/github-sign.png" alt="No image" /></a>
+                    <a href="https://www.linkedin.com/in/digeesh-s-013051272/" target="_blank"><img src="/linkedin.png" alt="No image" /></a>
+                    <a href="https://www.instagram.com/dige._.esx_/" target="_blank"><img src="/instagram.png" alt="No image" /></a>
+                    <a href="https://x.com/digeesh_2683" target="_blank"><img src="/twitter.png" alt="No image" /></a>
+                </div>
             </div>
         </div>
-    </div>
-)
+    )
 }
 
 export default Navbar
